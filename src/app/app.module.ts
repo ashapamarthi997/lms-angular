ee9ddbbfbb37fc2c1c0c486b93c232152cae8768
ashapamarthi997 <asha.pamarthi@toucanus.com>
2023-06-18 13:57:50 +0530

    welcome page with temp created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { TemperatureComponent } from './component/temperature/temperature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    TemperatureComponent
   ],
   imports: [
     BrowserModule,
