fbff01dc79a783989dafeea8d872ae6ca0d84ddc
ashapamarthi997 <asha.pamarthi@toucanus.com>
2023-06-25 10:14:06 +0530

    dash board

--- src/assets/temperature.js
+++ src/assets/temperature.js
@@ -1,4 +1,5 @@
- async function getTemperature(self) {
+ async function getTemperature(self) 
+ {
     let city = self.value
     let res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1`)
     let data = await res.json()
